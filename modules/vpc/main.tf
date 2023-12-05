4a301d25fcf33fcb3d4e34d0b26fecba024d8a58
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-05 06:36:16 +0530

    Prometheus ANsible Change

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
-  tags       = merge(var.tags, { Name = var.env})
+  tags       = merge(var.tags, { Name = var.env })
 }
 
-
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+}
 
