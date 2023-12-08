1b859533b74fd6983d3dd628541cee5ffd90ae33
Raghu K <raghuk.vit@gmail.com>
2023-12-08 06:25:19 +0530

    TF Code

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -63,7 +63,54 @@ resource "aws_autoscaling_group" "main" {
 }
 
 
-// role
+resource "aws_iam_role" "main" {
+  name = "${var.env}-${var.component}"
+  tags = merge(var.tags, { Name = "${var.env}-${var.component}" })
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "ssm_read_access"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "Get Resources",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource": "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component}.*"
+        },
+        {
+          "Sid": "List Resources",
+          "Effect": "Allow",
+          "Action": "ssm:DescribeParameters",
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+
+}
+
+
 
 
 
