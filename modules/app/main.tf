8b6146a98832f35a1a83e0c3e01d43a5192d8e8c
Raghu K <raghuk.vit@gmail.com>
2023-12-08 06:37:39 +0530

    TF Code

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -40,6 +40,9 @@ resource "aws_launch_template" "main" {
     role_name = var.component
     env       = var.env
   }))
+  iam_instance_profile {
+    name = aws_iam_instance_profile.main.name
+  }
 }
 
 resource "aws_autoscaling_group" "main" {
@@ -108,6 +111,11 @@ resource "aws_iam_role" "main" {
     })
   }
 
+}
+
+resource "aws_iam_instance_profile" "main" {
+  name = "${var.env}-${var.component}"
+  role = aws_iam_role.main.name
 }
 
 
