de89453c3291bae20e17e61013c254a3588cffc3
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-08 06:11:52 +0530

    Exit Status Check

--- main.tf
+++ main.tf
@@ -99,4 +99,18 @@ module "backend-alb" {
 }
 
 
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "load-gen" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-033d8567b50d2e180"]
+  tags = {
+    Name = "load-gen"
+  }
+}
 
